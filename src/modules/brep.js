a603c0a96df2a263157d4b2e5d8f1365427d0ce1
henrikrudstrom <hrudstrom@gmail.com>
2016-04-19 00:00:35 +0200

    topo properties

--- src/modules/brep.js
+++ src/modules/brep.js
@@ -7,17 +7,40 @@ module.exports = function(mod) {
   mod.include('TopLoc_Location');
   mod.include('TopAbs_Orientation');
   mod.include('TopoDS_Shape');
-  mod.include('TopoDS_Edge');
   mod.include('TopoDS_Vertex');
+  mod.include('TopoDS_Edge');
+  // mod.include('TopoDS_Wire');
+  // mod.include('TopoDS_Face');
+  // mod.include('TopoDS_Shell');
+  // mod.include('TopoDS_Solid');
+  // mod.include('TopoDS_Vertex');
+
+  mod.find('*').include('*');
   mod.includeBRepBuilder('BRepBuilderAPI_MakeEdge(Handle_Geom_*)', 'Edge');
   mod.includeBRepBuilder('BRepBuilderAPI_MakeVertex(*)', 'Vertex');
   mod.find('TopoDS_Shape')
     .rename('Location(TopLoc_Location)', 'SetLocation')
     .rename('Orientation(TopAbs_Orientation)', 'SetOrientation');
 
+  mod.find('TopoDS_*')
+    .exclude((decl) => decl.cls === 'constructor');
+
+  mod.find('TopoDS_Shape')
+    .property('Free()', 'Free(Standard_Boolean)')
+    .property('Locked()', 'Locked(Standard_Boolean)')
+    .property('Modified()', 'Modified(Standard_Boolean)')
+    .property('Checked()', 'Checked(Standard_Boolean)')
+    .property('Orientable()', 'Orientable(Standard_Boolean)')
+    .property('Closed()', 'Closed(Standard_Boolean)')
+    .property('Infinite()', 'Infinite(Standard_Boolean)')
+    .property('Convex()', 'Convex(Standard_Boolean)')
+
+    .property('Orientation()', 'Orientation(TopAbs_Orientation)')
+    .property('Location()', 'Location(TopLoc_Location)');
+
+
   mod.find('*').camelCase('*');
   mod.removePrefix('*');
-  mod.find('*').include('*');
+
   mod.noHandle('*');
-  mod.find('TopoDS_*').exclude((decl) => decl.cls === 'constructor');
 };
